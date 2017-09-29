fb84dc30e7d0aca0da944e01be0cc3d30db9c0cc
Shane Moodie <shanepmoodie96@gmail.com>
2017-09-29 15:11:06 -0400

    AlmostDone

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -8,9 +8,14 @@ namespace FortuneTeller
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to the mystical fortune telling fortune teller!");
+
+            Console.WriteLine("Please press enter to begin.");
+
+            Console.ReadKey(); 
 
             Console.WriteLine("Please tell me your first name.");
             string userFirst = Console.ReadLine();
@@ -24,34 +29,114 @@ namespace FortuneTeller
             Console.WriteLine("In what numerical month were you born?");
             int userBMonth = int.Parse(Console.ReadLine());
 
-            if (userBMonth == 1 || userBMonth == 2 || userBMonth == 3 || userBMonth == 4)
+            Console.WriteLine("What is your favorite ROYGBIV color? Enter \"Help\" for color repository.");
+            string userColor = Console.ReadLine().ToLower();
+             
+            if (userColor == "Help")
             {
-                Console.WriteLine("$150,000");
+                Console.WriteLine("Orange");
             }
-            else if (userBMonth == 5 || userBMonth == 6|| userBMonth == 7 || userBMonth == 8)
-            {
-                Console.WriteLine("$500,000");
-            }
-            else if (userBMonth == 9 || userBMonth == 10 || userBMonth == 11 || userBMonth == 12)
-            {
-                Console.WriteLine("$1,000,000 (Mwah-ha-ha!!!)");
-            }
-            else
-            {
-                Console.WriteLine("$0.00");
-            }
-
-            Console.WriteLine("What is your favorite ROYGBIV color? Enter \"HELP\" for color repository.");
-            string userColor = Console.ReadLine();
 
             Console.WriteLine("How many sibilings do you have?");
             int userSib = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Computing fortune...this is totally accurate...press enter when ready.");
+            Console.WriteLine("\a" + "Computing fortune...this is totally accurate...press enter when ready.");
 
-            //Console.WriteLine(userFirst + userLast "will retire in"
+            Console.ReadKey();
+
+            string retYear = "";
+
+            if (userAge % 2 == 0)
+            {
+                retYear = "20 years";
+            }
+            else
+            {
+                retYear = "30 years";
+            }
 
 
+            string vacaHome = "";
+
+            if (userSib == 0)
+            {
+                vacaHome = "The Citadel of Ricks";
+            }
+            else if (userSib == 1)
+            {
+                vacaHome = "Winterfell";
+            }
+            else if (userSib == 2)
+            {
+                vacaHome = "Rivendell";
+            }
+            else if (userSib == 3)
+            {
+                vacaHome = "Gotham City";
+            }
+            else if (userSib >= 4)
+            {
+                vacaHome = "Dr. Evil's Top Seceret Moon Base";
+            }
+            else if (userSib < 0)
+            {
+                vacaHome = "the dumpster behind Arby's";
+            }
+
+
+            string transport = "";
+
+            if (userColor == "red")
+            {
+                transport = "transportation gun";
+            }
+            else if (userColor == "orange")
+            {
+                transport = "a horse";
+            }
+            else if (userColor == "yellow")
+            {
+                transport = "a pogo stick"; 
+            }
+            else if (userColor == "green")
+            {
+                transport = "a Ford Model-T";
+            }
+            else if (userColor == "blue")
+            {
+                transport = "an old pair of Heelys";
+            }
+            else if (userColor == "indigo")
+            {
+                transport = "a skateboard missing one wheel";
+            }
+            else if (userColor == "violet")
+            {
+                transport = "a unicycle, complete with a dancing monkey wearing a hat";
+            }
+            
+
+            string dollaBillz = "";
+
+            if (userBMonth == 1 || userBMonth == 2 || userBMonth == 3 || userBMonth == 4)
+            {
+               dollaBillz = "$150,000"; 
+            }
+            else if (userBMonth == 5 || userBMonth == 6 || userBMonth == 7 || userBMonth == 8)
+            {
+                dollaBillz = "$500,000";
+            }
+            else if (userBMonth == 9 || userBMonth == 10 || userBMonth == 11 || userBMonth == 12)
+            {
+                dollaBillz = "$1,000,000";
+            }
+            else
+            {
+                dollaBillz = "$0.00";
+            }
+
+            Console.WriteLine(userFirst + " " + userLast + " will retire in " + retYear + " with " + dollaBillz + " in the bank, a vacation" + "\n" + "home in " + vacaHome + " and " + transport + ".");
+            
         }
     }
 }
